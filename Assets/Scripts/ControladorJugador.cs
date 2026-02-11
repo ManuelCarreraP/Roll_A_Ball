923d851ef0502c8c363a44e1aba87ddf324d5e75
ManuelCarreraP <mcarrerapazo@danielcastelao.org>
2026-02-11 10:53:45 +0100

    Recogida de puntos + Enemigo (Win y Lose)

--- Assets/Scripts/ControladorJugador.cs
+++ Assets/Scripts/ControladorJugador.cs
@@ -1,22 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb; 
-    private float movementX;
-    private float movementY;
-    public float speed = 0; 
+ private Rigidbody rb; 
 
-    void Start()
+ private int count;
+
+ private float movementX;
+ private float movementY;
+
+ public float speed = 0;
+
+ public TextMeshProUGUI countText;
+
+ public GameObject winTextObject;
+
+ void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
     }
  
-    void OnMove(InputValue movementValue)
+ void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -24,10 +36,50 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y; 
     }
 
-    private void FixedUpdate() 
+ private void FixedUpdate() 
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        
+
         rb.AddForce(movement * speed); 
     }
+
+ 
+ void OnTriggerEnter(Collider other) 
+    {
+ if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+ void SetCountText() 
+    {
+        countText.text = "Count: " + count.ToString();
+
+ if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+private void OnCollisionEnter(Collision collision)
+{
+ if (collision.gameObject.CompareTag("Enemy"))
+    {
+        Destroy(gameObject); 
+ 
+        winTextObject.gameObject.SetActive(true);
+        winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+ 
+    }
+
+}
+
+
 }
